423859c55ca9c7f7c2aa2daf0e4611709e55a567
bianliao <138651187+bianliao@users.noreply.github.com>
2025-03-10 21:46:47 +0800

    build db completed

--- data/taipei-attractions.py
+++ data/taipei-attractions.py
@@ -17,34 +17,48 @@ def get_db_connection():
     )
 
 # built db structure
-with get_db_connection() as db:
-        with db.cursor(dictionary=True) as cursor:
-            cursor.execute("""CREATE TABLE attractions(
-                id INT PRIMARY KEY AUTO_INCREMENT,
-                name VARCHAR(255) NOT NULL,
-                mrt VARCHAR(255) NOT NULL,
-                serial_no INT NOT NULL,
-                image_url VARCHAR(2000) NOT NULL,
-                description TEXT NOT NULL)
-            """)
-            cursor.execute("INSERT INTO attractions(name, mrt, serial_no, image_url, description) VALUES ('test','test2',123,'test4','test5'); ")
-            db.commit()
+# with get_db_connection() as db:
+#         with db.cursor(dictionary=True) as cursor:
+#             cursor.execute("""CREATE TABLE attractions(
+#                 id INT PRIMARY KEY AUTO_INCREMENT,
+#                 name VARCHAR(255) NOT NULL,
+#                 rate INT NOT NULL DEFAULT 0 ,
+#                 mrt VARCHAR(255) NOT NULL,
+#                 serial_no BIGINT NOT NULL
+#                 image_url TEXT NOT NULL,
+#                 description TEXT NOT NULL)
+#             """)
+#             cursor.execute("INSERT INTO attractions(name, mrt, serial_no, image_url, description) VALUES ('test','test2',123,'test4','test5'); ")
+#             db.commit()
 
 
 # import json into db
-with open("data/taipei-attractions.json", mode="r") as file:
-    data =json.load(file)
-    attractions = data['result']['results']
-    for attraction in attractions:
-        rate = attraction["rate"]
-        SpotTitle = attraction["name"]
-        Mrt = attraction["MRT"]
-        Serial_NO = attraction["SERIAL_NO"]
-        Description = attraction["description"]
-        Image_URLs = attraction["file"].split("http")[1:]
-        Image_URLs =[ f"http{url}" for url in Image_URLs]
-        # print(Image_URLs) 
-    # taipei_attractions = {attraction["name"]:attractions for attraction in attractions}
-    # print(taipei_attractions)
+# with open("data/taipei-attractions.json", mode="r") as file:
+#     data =json.load(file)
+#     attractions = data['result']['results']
+
+#     with get_db_connection() as db:
+#         with db.cursor(dictionary=True) as cursor:
+              
+#             for attraction in attractions:
+#                 rate = attraction.get("rate", 0)
+#                 SpotTitle = attraction["name"]
+#                 Mrt = attraction.get("MRT", None)
+#                 if Mrt is None:
+#                     Mrt = "Unknown" 
+#                 Serial_NO = attraction["SERIAL_NO"]
+#                 Description = attraction["description"]
+#                 Image_URLs = attraction["file"].split("http")[1:]
+#                 Image_URLs =[ f"http{url}" for url in Image_URLs]
+#                 Image_URLs_str = ",".join(Image_URLs)
+#                 # print(Image_URLs_str) 
+#                 # print(SpotTitle, Mrt, Serial_NO)
+
+#                 cursor.execute("""
+#                     INSERT INTO attractions (rate, name, mrt, serial_no, image_url, description)
+#                     VALUES (%s, %s, %s, %s, %s, %s);
+#                 """,(rate, SpotTitle, Mrt, Serial_NO, Image_URLs_str, Description))
+#             db.commit()
+
 
         
