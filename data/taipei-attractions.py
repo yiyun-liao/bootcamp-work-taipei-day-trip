f59f2f63dd9f483ae4f0ab66f69f50df26e0e067
bianliao <138651187+bianliao@users.noreply.github.com>
2025-03-10 20:55:59 +0800

    open json and connect db

--- data/taipei-attractions.py
+++ data/taipei-attractions.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
@@ -12,11 +13,38 @@ def get_db_connection():
         user="root",
         password=PASSWORD,
         host="localhost",
-        database="website"
+        database="taipei_attractions"
     )
 
+# built db structure
 with get_db_connection() as db:
         with db.cursor(dictionary=True) as cursor:
-            cursor.execute("SELECT * FROM member;")
-            test = cursor.fetchall()
-            print(test)
+            cursor.execute("""CREATE TABLE attractions(
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                name VARCHAR(255) NOT NULL,
+                mrt VARCHAR(255) NOT NULL,
+                serial_no INT NOT NULL,
+                image_url VARCHAR(2000) NOT NULL,
+                description TEXT NOT NULL)
+            """)
+            cursor.execute("INSERT INTO attractions(name, mrt, serial_no, image_url, description) VALUES ('test','test2',123,'test4','test5'); ")
+            db.commit()
+
+
+# import json into db
+with open("data/taipei-attractions.json", mode="r") as file:
+    data =json.load(file)
+    attractions = data['result']['results']
+    for attraction in attractions:
+        rate = attraction["rate"]
+        SpotTitle = attraction["name"]
+        Mrt = attraction["MRT"]
+        Serial_NO = attraction["SERIAL_NO"]
+        Description = attraction["description"]
+        Image_URLs = attraction["file"].split("http")[1:]
+        Image_URLs =[ f"http{url}" for url in Image_URLs]
+        # print(Image_URLs) 
+    # taipei_attractions = {attraction["name"]:attractions for attraction in attractions}
+    # print(taipei_attractions)
+
+        
