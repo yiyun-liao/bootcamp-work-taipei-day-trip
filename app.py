f8dcff5b4f714aac36f82a5850e1a7ebcbf627c9
bianliao <138651187+bianliao@users.noreply.github.com>
2025-03-11 15:35:03 +0800

    /api/attraction/{attractionId}

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import *
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 import mysql.connector
 from dotenv import load_dotenv
 import os
@@ -22,57 +22,91 @@ def get_db_connection():
 
 @app.get("/api/attractions")
 def get_attractions(
-	page: Annotated[int,Query(ge=0)] = 0,
+	page: Annotated[int,Query(ge=0)],
 	keyword: Optional[str] = None
 	):
+	try:
+		search = "SELECT * FROM attractions"
 
-	search = "SELECT * FROM attractions"
+		# 分頁回傳
+		limit = 12
+		offset = limit * page
 
-	# 分頁回傳
-	limit = 12
-	offset = limit * page
-
-	# 檢查是否有下頁資料
-	next_page = None
-	next_page_offset = limit * (page + 1)
-
-	# 模糊查詢
-	if keyword:
-		search += " WHERE mrt = %s OR name LIKE %s"
-		search += " LIMIT %s OFFSET %s;"
-		params = (keyword, f"%{keyword}%", limit, offset)
-		next_page_params = (keyword, f"%{keyword}%", limit, next_page_offset)
-	else:
-		search += " LIMIT %s OFFSET %s;"
-		params = (limit, offset)	
-		next_page_params = (limit, next_page_offset)
-	
-	print(f"cursor.execute({search},{params})" )
-	print(f"cursor.execute({search},{next_page_params})" )
-
-	db = get_db_connection()
-	cursor = db.cursor(dictionary=True)
-	cursor.execute(search,params)
-	data = cursor.fetchall()
-	cursor.execute(search,next_page_params)
-	next_page_data = cursor.fetchall()
-	cursor.close()
-	db.close()
-
-	for img in data:
-		img["images"] = json.loads(img["images"])
-
-	if next_page_data:
-		next_page = page + 1
-	else:
+		# 檢查是否有下頁資料
 		next_page = None
+		next_page_offset = limit * (page + 1)
 
-	return{
-		"nextPage":next_page,
-		"data": data}
+		# 模糊查詢
+		if keyword:
+			search += " WHERE mrt = %s OR name LIKE %s"
+			search += " LIMIT %s OFFSET %s;"
+			params = (keyword, f"%{keyword}%", limit, offset)
+			next_page_params = (keyword, f"%{keyword}%", limit, next_page_offset)
+		else:
+			search += " LIMIT %s OFFSET %s;"
+			params = (limit, offset)	
+			next_page_params = (limit, next_page_offset)
+		
+		print(f"cursor.execute({search},{params})" )
+		print(f"cursor.execute({search},{next_page_params})" )
+
+		db = get_db_connection()
+		cursor = db.cursor(dictionary=True)
+		cursor.execute(search,params)
+		data = cursor.fetchall()
+		cursor.execute(search,next_page_params)
+		next_page_data = cursor.fetchall()
+		cursor.close()
+		db.close()
+
+		for img in data:
+			img["images"] = json.loads(img["images"])
+
+		if next_page_data:
+			next_page = page + 1
+		else:
+			next_page = None
+
+		return{
+			"nextPage":next_page,
+			"data": data}
+	except Exception:
+		print("error")
+		return{
+			"error": True,
+			"message": "請按照情境提供對應的錯誤訊息"
+		}
 
 
+@app.get("/api/attraction/{attractionId}")
+def get_attractions(attractionId=int):
+	try:		
+		print(attractionId )
 
+		db = get_db_connection()
+		cursor = db.cursor(dictionary=True)
+		cursor.execute("SELECT * FROM attractions WHERE id = %s",(attractionId,))
+		data = cursor.fetchall()
+		cursor.close()
+		db.close()
+
+		if not data:
+			return JSONResponse(content={
+				"error": True,
+				"message": "請按照情境提供對應的錯誤訊息"
+			}, status_code=400)
+
+		for img in data:
+			img["images"] = json.loads(img["images"])
+
+		return{
+			"data": data}
+	except Exception:
+		print("error")
+		return{
+			"error": True,
+			"message": "請按照情境提供對應的錯誤訊息"
+		}	
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
