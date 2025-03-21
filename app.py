3c56f4f4734cd040615d701a4cffda0dc9ca9340
bianliao <138651187+bianliao@users.noreply.github.com>
2025-03-21 15:44:39 +0800

    connect to ec2 mysql

--- app.py
+++ app.py
@@ -11,27 +11,27 @@ app=FastAPI()
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-# PASSWORD = os.getenv("PASSWORD")
-
-# def get_db_connection():
-#     return mysql.connector.connect(
-#         user="root",
-#         password=PASSWORD,
-#         host="35.75.244.94",
-#         database="taipei_attractions",
-# 		connect_timeout=10  # 設定 10 秒超時
-#     )
-
-PASSWORD = os.getenv("MYSQL_LOCAL_PASSWORD")
+PASSWORD = os.getenv("PASSWORD")
 
 def get_db_connection():
     return mysql.connector.connect(
         user="root",
         password=PASSWORD,
-        host="localhost",
+        host="35.75.244.94",
         database="taipei_attractions",
 		connect_timeout=10  # 設定 10 秒超時
     )
+
+# PASSWORD = os.getenv("MYSQL_LOCAL_PASSWORD")
+
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         user="root",
+#         password=PASSWORD,
+#         host="localhost",
+#         database="taipei_attractions",
+# 		connect_timeout=10 
+#     )
 
 @app.get("/api/attractions")
 def get_attractions(
