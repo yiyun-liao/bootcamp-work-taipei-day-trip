238c2dd76561933042f7a1ed6ea607fa2be81c35
bianliao <138651187+bianliao@users.noreply.github.com>
2025-03-25 16:33:06 +0800

    slideBtnState()

--- static/script/components/Slide.js
+++ static/script/components/Slide.js
@@ -1,41 +1,53 @@
-export function slide(){
-    // const carouselContainer = document.querySelector('#carousel-container');
-    // const carouselList = carouselContainer.querySelector('ol');
-    // const carouselRightBtn = document.getElementById('carousel-right-btn');
-    // const carouselLeftBtn = document.getElementById('carousel-left-btn');
-    
-    // function carouselBtnState(){
-    //     let carouselContainerTotalWidth = carouselList.scrollWidth;
-    //     let carouselContainerWidth = carouselContainer.clientWidth;
-    //     let carouselOffsetLeft = carouselList.scrollLeft;
+export function slide(slideArray, location){
+    const slideImg = document.querySelector(`#${location}`);
+    slideImg.style.backgroundImage = `url("${slideArray[0] || ''}")`;
 
-    //     // console.log("總長度", carouselContainerTotalWidth)
-    //     // console.log("可視範圍", carouselContainerWidth)
-    //     // console.log("偏移位置", carouselOffsetLeft)
-
-    //     // 修正小數點誤差，確保按鈕顯示正確
-    //     const isRightBtnHide = Math.ceil(carouselOffsetLeft + carouselContainerWidth) >= carouselContainerTotalWidth;
-    //     const isLeftBtnHide = Math.ceil(carouselOffsetLeft) <= 0;
-        
-    //     if (carouselContainerTotalWidth > carouselContainerWidth){
-    //         carouselRightBtn.style.display = isRightBtnHide  ? 'none' : 'block' ;
-    //         carouselLeftBtn.style.display = isLeftBtnHide ? 'none' : 'block' ;
-    //     }else{
-    //         carouselRightBtn.style.display = "none";
-    //         carouselLeftBtn.style.display = "none";
-    //     }
-    // }
+    let slideTotal = slideArray.length;
+    let currentSlideImg = 0;
+    console.log(slideArray)
+    console.log('current', currentSlideImg, 'slideTotal', slideTotal)
+    console.log(`#${location} .slide-left-btn`)
     
-    // carouselLeftBtn.addEventListener('click', function(){
-    //     carouselList.scrollBy({left: -carouselContainer.clientWidth * 0.8, behavior: "smooth"});
-    //     setTimeout(carouselBtnState, 300); // 等滾動完成後再檢查按鈕狀態
-    //     // console.log("Left button clicked!");
-    // });
-    // carouselRightBtn.addEventListener('click', function(){
-    //     carouselList.scrollBy({left: carouselContainer.clientWidth * 0.8, behavior: "smooth"});
-    //     setTimeout(carouselBtnState, 300); // 等滾動完成後再檢查按鈕狀態
-    //     // console.log("Right button clicked!");
-    // });
-    // window.addEventListener('resize', carouselBtnState); // 新增只要畫面尺寸不同就改變，不需要整頁 refresh
-    // carouselBtnState();
+    const leftBtn = document.querySelector(`#${location} .slide-left-btn`)
+    const rightBtn = document.querySelector(`#${location} .slide-right-btn`)
+
+    leftBtn.addEventListener('click', function(){
+        if (currentSlideImg > 0){
+            currentSlideImg --;
+            console.log('after click left button, current', currentSlideImg, 'slideTotal', slideTotal)
+            slideImg.style.backgroundImage = `url("${slideArray[currentSlideImg] || ''}")`;
+            slideBtnState()
+        }
+    })
+
+    rightBtn.addEventListener('click', function () {
+        if (currentSlideImg < slideTotal -1) {
+            currentSlideImg++;
+            console.log('after click right button, current', currentSlideImg, 'slideTotal', slideTotal)
+            slideImg.style.backgroundImage = `url("${slideArray[currentSlideImg] || ''}")`;
+            slideBtnState(); // 更新按鈕顯示
+        }
+    });
+
+
+
+    function slideBtnState(){
+        console.log('slideBtnState', currentSlideImg, 'slideTotal', slideTotal)
+        if (slideTotal <= 0) return;
+        if (currentSlideImg <= 0){
+            leftBtn.style.display = 'none';
+            rightBtn.style.display = 'block';
+        }else if (0 < currentSlideImg && currentSlideImg < slideTotal - 1){
+            leftBtn.style.display = 'block';
+            rightBtn.style.display = 'block';
+        }else if (currentSlideImg === slideTotal - 1){
+            leftBtn.style.display = 'block';
+            rightBtn.style.display = 'none';
+        }else{
+            leftBtn.style.display = 'none';
+            rightBtn.style.display = 'none';            
+        }
+    }
+
+    slideBtnState()
 }
--- static/script/feature/ScriptAttraction.js
+++ static/script/feature/ScriptAttraction.js
@@ -1,5 +1,5 @@
 import { fetchData } from "../components/FetchData.js"
-
+import { slide } from "../components/Slide.js";
 
 export async function getAttractionDetails(id){
     const url = `/api/attraction/${id}`;
@@ -8,15 +8,14 @@ export async function getAttractionDetails(id){
 };
 
 function renderAttractionPage (attractionPage){
-    const slide = document.querySelector('.attraction-slide');
-    const name = document.querySelector('.attraction-simple-intro h3');
-    const category = document.querySelector('.attraction-simple-intro p');
-    slide.style.backgroundImage = `url('${attractionPage.images[0] || ""}')`;
-    name.textContent = attractionPage.name || '';
-    category.textContent = attractionPage.category || '';
+    const slideName = document.querySelector('.attraction-simple-intro h3');
+    const slideCategory = document.querySelector('.attraction-simple-intro p');
+    slideName.textContent = attractionPage.name || '';
+    slideCategory.textContent = attractionPage.category || '';
     
-    const desc = [attractionPage.description, attractionPage.address, attractionPage.transport];
+    const slideDesc = [attractionPage.description ?? '', attractionPage.address ?? '', attractionPage.transport ?? ''];
     document.querySelectorAll('.attraction-desc-p').forEach((p, index) => { 
-        p.textContent = desc[index] || '' ; 
-    })
+        p.textContent = slideDesc[index] || '' ; 
+    });
+    slide(attractionPage.images, 'attraction-slide');
 }
