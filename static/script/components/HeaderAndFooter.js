af894d710f281902eb7c5fd0acaf9c253cf5b9c7
bianliao <138651187+bianliao@users.noreply.github.com>
2025-03-31 23:41:19 +0800

    add header.html

--- static/script/components/HeaderAndFooter.js
+++ static/script/components/HeaderAndFooter.js
@@ -1,23 +1,28 @@
 export function renderHeaderAndFooter(){
-    const header = document.createElement('header');
+    fetch("static/header.html")
+        .then(response => response.text())
+        .then(html =>{
+            document.body.insertAdjacentElement('afterbegin', html);
+        })
+    // const header = document.createElement('header');
+    // header.innerHTML=`
+    //     <nav>
+    //         <h2>台北一日遊</h2>
+    //         <div>
+    //             <button class="body-med btn-med-plain">預定行程</button>
+    //             <button class="body-med btn-med-plain">登入/註冊</button>
+    //         </div>
+    //     </nav>
+    // `;
+    // document.body.prepend(header);
+    document.body.appendChild(footer);
+    
     const footer = document.createElement('footer');
-    header.innerHTML=`
-        <nav>
-            <h2>台北一日遊</h2>
-            <div>
-                <button class="body-med btn-med-plain">預定行程</button>
-                <button class="body-med btn-med-plain">登入/註冊</button>
-            </div>
-        </nav>
-    `;
     footer.innerHTML=`
         <div>
             <p class="body-bold">COPYRIGHT © 2021 台北一日遊</p>
         </div>
     `;
-    document.body.prepend(header);
-    document.body.appendChild(footer);
-
     document.querySelector('header nav h2').addEventListener('click', ()=>{
         window.location.href='/';
     })
