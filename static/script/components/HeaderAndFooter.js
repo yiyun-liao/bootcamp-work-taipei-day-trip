e444d0dccca9663cdde6d07630efdd1f2477e38e
bianliao <138651187+bianliao@users.noreply.github.com>
2025-04-01 01:10:29 +0800

    pop-up style

--- static/script/components/HeaderAndFooter.js
+++ static/script/components/HeaderAndFooter.js
@@ -1,9 +1,30 @@
-export function renderHeaderAndFooter(){
-    fetch("static/header.html")
-        .then(response => response.text())
-        .then(html =>{
-            document.body.insertAdjacentElement('afterbegin', html);
-        })
+export async function renderHeaderAndFooter(){
+    try{
+        const response = await fetch("/static/header.html")
+        const html = await response.text();
+        console.log('Fetched header:', html);
+        document.body.insertAdjacentHTML('afterbegin', html);
+        setHeaderAction()
+    } catch(error){
+        console.error('Error fetching data:', error);
+    }
+        
+    const footer = document.createElement('footer');
+    footer.innerHTML=`
+    <div>
+    <p class="body-bold">COPYRIGHT © 2021 台北一日遊</p>
+    </div>
+    `;
+    document.body.appendChild(footer);
+    
+    }
+    
+function setHeaderAction(){
+    document.querySelector('header nav h2').addEventListener('click', ()=>{
+        window.location.href='/';
+    })
+    
+}
     // const header = document.createElement('header');
     // header.innerHTML=`
     //     <nav>
@@ -15,15 +36,3 @@ export function renderHeaderAndFooter(){
     //     </nav>
     // `;
     // document.body.prepend(header);
-    document.body.appendChild(footer);
-    
-    const footer = document.createElement('footer');
-    footer.innerHTML=`
-        <div>
-            <p class="body-bold">COPYRIGHT © 2021 台北一日遊</p>
-        </div>
-    `;
-    document.querySelector('header nav h2').addEventListener('click', ()=>{
-        window.location.href='/';
-    })
-}
