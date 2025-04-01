d5c80bd558e00b2bdd50b533aec9772adc2ba6c4
bianliao <138651187+bianliao@users.noreply.github.com>
2025-04-01 10:30:27 +0800

    popup open,switch and close

--- static/script/components/HeaderAndFooter.js
+++ static/script/components/HeaderAndFooter.js
@@ -1,20 +1,22 @@
+import { login } from "./LoginAndSignup.js";
+
 export async function renderHeaderAndFooter(){
     try{
         const response = await fetch("/static/header.html")
         const html = await response.text();
-        console.log('Fetched header:', html);
         document.body.insertAdjacentHTML('afterbegin', html);
-        setHeaderAction()
+        setHeaderAction();
+        login()
     } catch(error){
         console.error('Error fetching data:', error);
     }
         
     const footer = document.createElement('footer');
     footer.innerHTML=`
-    <div>
-    <p class="body-bold">COPYRIGHT © 2021 台北一日遊</p>
-    </div>
-    `;
+        <div>
+        <p class="body-bold">COPYRIGHT © 2021 台北一日遊</p>
+        </div>
+        `;
     document.body.appendChild(footer);
     
     }
@@ -23,16 +25,4 @@ function setHeaderAction(){
     document.querySelector('header nav h2').addEventListener('click', ()=>{
         window.location.href='/';
     })
-    
 }
-    // const header = document.createElement('header');
-    // header.innerHTML=`
-    //     <nav>
-    //         <h2>台北一日遊</h2>
-    //         <div>
-    //             <button class="body-med btn-med-plain">預定行程</button>
-    //             <button class="body-med btn-med-plain">登入/註冊</button>
-    //         </div>
-    //     </nav>
-    // `;
-    // document.body.prepend(header);
