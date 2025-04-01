b1582b838cf24a2e3ef96a91bebd999f927e6448
bianliao <138651187+bianliao@users.noreply.github.com>
2025-04-01 14:57:14 +0800

    /api/user/auth

---
+++ static/script/components/CheckTokenValid.js
@@ -0,0 +1,14 @@
+export async function checkTokenValid(){
+    const token = localStorage.getItem('token');
+    const response = await fetch('/api/user/auth',{
+        method: 'GET',
+        headers:{'Authorization': `Bearer ${token}`},
+    });
+    const data = await response.json();
+    console.log(data)
+    if (response.status === 200){
+        console.log("用戶資料:", data);
+    }else {
+        console.log("登入失敗或無效的 token");        
+    }
+}
