d81b8613c726a61662ed8a66ff19a35c339e164d
bianliao <138651187+bianliao@users.noreply.github.com>
2025-04-16 21:24:03 +0800

    week13

---
+++ data/order-data.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+PASSWORD = os.getenv("PASSWORD")
+
+def get_db_connection():
+    return mysql.connector.connect(
+        user="root",
+        password=PASSWORD,
+        host="35.75.244.94",
+        database="taipei_attractions"
+    )
